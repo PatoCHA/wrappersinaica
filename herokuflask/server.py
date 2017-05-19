fd1e68893b32b09dfcc6c63c4377168ee96a6abc
PatoCHA <patriciojavier15@gmail.com>
2017-05-18 23:05:03 -0500

    Update server.py

--- herokuflask/server.py
+++ herokuflask/server.py
@@ -3,7 +3,6 @@ import sys #ver mejor los errores
 import os #sirve para usar el puerto abierto en heroku
 from flask import Flask, request 
 from flask_restful import Resource, Api
-from json import dumps
 from flask_jsonpify import jsonify
 import requests
 from bs4 import BeautifulSoup
